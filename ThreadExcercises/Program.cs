804658d34b36c0e5847f2212b065d8042b3586a8
MatteoLucchiPascal <mlucchi79@gmail.com>
2020-11-11 08:54:27 +0100

    Esercizio thread

---
+++ ThreadExcercises/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace ThreadExcercises
+{
+    class Program
+    {
+        static int count = 0;
+        
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(lettore));
+
+            t1.Start();
+
+            t1.IsBackground = true;
+
+            string parola = "c";
+            StreamReader documento = new StreamReader("input.txt");
+            while (parola != null)
+            {
+
+                parola = documento.ReadLine();
+
+                if (parola == "Thread")
+                    count++;
+
+                Thread.Sleep(1);
+
+            }
+
+           
+
+        }
+
+        static void lettore()
+        {
+            while(true)
+            {
+                Console.WriteLine(count);
+                Thread.Sleep(1);
+
+            }
+           
+
+        }
+    }
+}
